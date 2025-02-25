8843c0b0a9d6d68283d8c571ef7ba3d3bd7380e0
devrawhul <iamrahulsh02@gmail.com>
2025-02-26 02:53:56 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/redsamurai/',
   server: {
     host: true, // Exposes to the network automatically
     port: 5173, // Change this if needed
     strictPort: true, // Ensures it doesn't change the port
+  },
+  build: {
+    outDir: 'build'
   }
+
 })
